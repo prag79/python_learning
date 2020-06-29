732f5306f05cdb8944fd88c06ecc3ae38c647efa
Pragnajit <prag79@gmail.com>
2020-06-29 10:50:19 +0530

    sum of variables

---
+++ PythonLearning.py
@@ -0,0 +1,18 @@
+def addOne(Number):
+    Output = Number + 1.3
+    return Output
+Var =0
+print(Var)
+Var2 = addOne(Var)
+Var3 = addOne(Var2)
+print(Var2)
+print(Var3)
+Var4 = addOne(2.5 +3.4)
+print(Var4)
+def addOneAddTwo(NumberOne, NumberTwo):
+    Output = NumberOne+1
+    Output += NumberTwo +2
+    return Output
+
+Sum = addOneAddTwo(1,2)
+print(Sum)
